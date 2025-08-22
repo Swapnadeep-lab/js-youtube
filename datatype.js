cabc53244443d9c2c207d23737a21dc338bdfd56
Swapnadeep-lab <swapnadeepm204@gmail.com>
2025-08-22 15:32:27 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,62 @@
+// console.log(+true);
+// console.log(+"");
+// let num1, num2, num3
+// num1= num2= num3= 2+2
+// let gamecounter=100
+// gamecounter++;
+// console.log(gamecounter);
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// Primitive 
+// 7 types: string, number, boolean, null, undefined, symbol
+
+// const score= 100
+// const scoreValue= 100.83
+// const isLoggedIn= false
+// const outsideTemp= null
+// let userEmail;
+
+// const id= Symbol('123')
+// const anotherId= Symbol('123')
+// console.log(id=== anotherId);
+// const bigNumber= 87876576878n
+// // references (Non primitive)
+// // Array, objects, function
+// const heros= ["shaktiman", "swapnadeep", "dogs"]
+// let myObj=
+// {
+//   name: "swapna",
+//   age: 22,
+
+// }
+
+// let myYoutubename= "swapnadeepmondal"
+// let anothername="myYoutubename"
+// console.log(anothername);
+
+// let user={
+//   email: "swapnadeepm204@gmail.com",
+//   upi: "user@ybl"
+// }
+
+// let userTwo= userOne
+
+// userTwo.email="hitesh@gmail.com"
+// console.log(userOne.email);
+// console.log(userTwo.email);
+// const name= "hitesh"
+// const repoCount=50
+// // console.log(name+ repoCount+ "value");
+// console.log('Hello my name is ${name} and my repo count is $ {repoCount}');
+
+import java.util.*;
+public class datatype{
+  public static void ptintMyName(String args[]){
+    
+  }
+}
